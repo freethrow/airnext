ccd47d0dde7b21b4512b15172e777974c519e347
freethrow <aleksendric@gmail.com>
2023-02-25 19:11:50 +0100

    first commit - setup Nextjs project, add Tailwind

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,12 @@
 import '@/styles/globals.css'
 
+import Navbar from '@/components/Navbar'
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+  <div className="min-h-screen border border-gray-400 text-gray-600">
+    <Navbar />
+    <Component {...pageProps} />
+  </div>
+  )  
 }
